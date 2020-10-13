9e6499bb9265ad8eaf8b62568cd4a2736ea90638
Brian Lee <the930325@yahoo.com>
2020-10-12 22:41:31 -0400

    variables added to dfs of 3 species

--- Thrust.py
+++ Thrust.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import numpy as np
+import re
 
 # global variables
 
-f_path = "/Users/beelee/Desktop/10-07-data.csv"
+f_path = "/Users/beelee/Desktop/Jelly/F20/2001_data/csv/"
+a_digitale_f = f_path + "a_digitale_data.csv"
+a_victoria_f = f_path + "a_victoria_data.csv"
+m_cellularia_f = f_path + "m_cellularia_data.csv"
 
 vel_o = 0
 acc_m = 0
 acc_o = 0
 
-h_b = 0.0
-d_b = 0.0
 s_b = 0.0
 
 t_rel = 0
@@ -28,40 +30,117 @@ acc_cor = 0
 
 
 def main():
-    # import csv into 2 dataframes, 1 prolate 1 oblate
-    df = pd.read_csv(f_path)
-    dfs = np.split(df, [26], axis=1)  # may need a better system splitting
 
-    # dfs[0].drop(list(range(41, 46)), axis=0, inplace=True)
-    dfs[0].dropna(axis=1, how='all', inplace=True)
-    dfs[1].drop(list(range(36, 46)), axis=0, inplace=True)
-    dfs[1].drop("Unnamed: 27", axis=1, inplace=True)
-    dfs[1].dropna(axis=1, how='all', inplace=True)
-    # print(dfs[0].shape, dfs[1].shape)
-    print(dfs[:])
+    # import pre-cleaned up csv files, 1 prolate 2 oblates
+    a_digitale = pd.read_csv(a_digitale_f)
+    a_victoria = pd.read_csv(a_victoria_f)
+    m_cellularia = pd.read_csv(m_cellularia_f)
+    dfs = [a_digitale, a_victoria, m_cellularia]
 
-    time_pro = {'time': np.arange(0.05, 2.30, 0.05)}
-    a_digitale = pd.DataFrame(time_pro, columns=['time', 'height', 'diameter'])
+    # clean up dataframes
 
-    for time in list(range(len(dfs[0].index)-1)):
-        u = dfs[0].loc[time + 1].v / 100.0  # m^2/s
-        dim(dfs[0].loc[time].re, u, dfs[0].loc[time].f)  # re: m^2/s / m^2/s, fineness: m/m
-        # print('height %f and diameter %f at %f' % (h_b, d_b, (time + 1) * 0.05))
-        a_digitale.loc[a_digitale.index[time], 'height'] = h_b
-        a_digitale.loc[a_digitale.index[time], 'diameter'] = d_b
+    a_digitale.dropna(axis=0, how='any', inplace=True)  # drop incomplete rows for the prolate medusae
+    m_cellularia.dropna(axis=0, how='any', inplace=True)  # drop incomplete rows for the oblate medusae
 
-    # time_obl = {'time': np.arange(1 / 6, 37 / 6, 1 / 6)}
-    # a_victoria = pd.DataFrame(time_obl, columns=['time', 'height', 'diameter'])
-    # for time in list(range(len(dfs[1].index)-1)):
-    #     u = dfs[1].loc[time].'v.3' / 100.0  # m^2/s
-    #     print(u)
-    #     dim(dfs[1].loc[time].'re.3', u, dfs[1].loc[time].'f.3')  # re: m^2/s / m^2/s, fineness: m/m
-    #     print('height %f and diameter %f at %i' % (h_b, d_b, time))
-    #     a_victoria.loc[a_digitale.index[time], 'height'] = h_b
-    #     a_victoria.loc[a_digitale.index[time], 'diameter'] = d_b
-    #
-    # # print(a_digitale)
-    print(a_digitale)
+    # for column in dfs_2001[0].columns:  # shift velocity columns to line up data for prolate medusae
+    #     index_no = dfs_2001[0].columns.get_loc(column)
+    #     if index_no == 5 or index_no == 6:
+    #         dfs_2001[0][column] = dfs_2001[0][column].shift(-1)
+
+    # deleate extra references
+    for df in dfs:
+
+        to_delete = []  # store columns to delete as a list
+        for column in df.columns:
+            index_no = df.columns.get_loc(column)  # get the column index number
+            # print("column #%i" % index_no)
+
+            if index_no % 2 == 1:
+                eq = True
+                second_err = False  # only cancel deletion when 2 time references in a row are different from template
+                # print("eq set to true")
+                for row in df.index:
+                    if np.absolute(df.iat[row, 0] - round(df.iat[row, index_no], 2)) > 0.02:
+                        # print("row value %f and column value %f" % (df.iat[row, 0], round(df.iat[row, index_no], 2)))
+                        if second_err:
+                            eq = False
+                            # print("eq turns false")
+                            break
+                        second_err = True
+                    else:
+                        second_err = False
+            else:
+                eq = False
+                # print("eq set to false")
+
+            if eq:
+                # print("eq true all the way at column %i, so store name" % index_no)
+                to_delete.append(column)
+
+        if to_delete:
+            df.drop(to_delete, axis=1, inplace=True)
+
+    for df in dfs:
+        f_col = ''
+        vel_col = ''
+        re_col = ''
+
+        for column in df.columns:
+            if re.search(r'f', column):
+                f_col = re.search(r'f', column).string
+                # print(f_col)
+            if re.search(r'v', column):
+                vel_col = re.search(r'v', column).string
+                # print(vel_col)
+            if re.search(r're', column):
+                re_col = re.search(r're', column).string
+                # print(re_col)
+
+        velocities = []  # store instantaneous velocities with newly converted units
+        heights = []  # store instantaneous heights
+        diameters = []  # store instantaneous diameters
+
+        for row in df.index:
+            u = df.at[row, vel_col] / 100.0  # convert velocity unit to m^2/s
+            velocities.append(u)
+            d_h = dim(df.at[row, re_col], u, df.at[row, f_col])  # re: m^2/s / m^2/s, fineness: m/m
+            diameters.append(d_h[0])
+            heights.append(d_h[1])
+
+        df["u"] = velocities
+        df["h"] = heights
+        df["d"] = diameters
+
+
+
+    for df in dfs:
+
+        volumes = []  # store instantaneous volumes
+        masses = []  # store instantaneous masses
+        orifices = []  # store instantaneous orifices
+        drags = []  # store instantaneous drags
+        net_forces = []  # store instantaneous net_forces
+        thrusts = []  # store instantaneous thrusts
+
+        for row in df.index:
+            h = df.at[row, 'h']
+            d = df.at[row, 'd']
+            u = df.at[row, 'u']
+            volumes.append(vol(h, d))
+            masses.append(mas(h, d))
+            orifices.append(ori(d))
+            drags.append(drg(h, d, u))
+            net_forces.append(nfr_m(h, d))
+            thrusts.append(thr_m(h, d, u))
+
+        df["vol"] = volumes
+        df["mass"] = masses
+        df["ori"] = orifices
+        df["drag"] = drags
+        df["modeled_net"] = net_forces
+        df["modeled thrust"] = thrusts
+
+        print(df)
 
 
 
@@ -71,10 +150,9 @@ def main():
 # param: Re, fineness
 ######################################################################
 def dim(re_ref, u_ref, f_ref):
-    global d_b
-    d_b = 1.0 * re_ref * sea_vis / u_ref  # diameter: m
-    global h_b
+    d_b = 1.0 * re_ref * sea_vis / np.absolute(u_ref)  # diameter: m
     h_b = d_b * f_ref  # height: m
+    return [d_b, h_b]
 
 
 ######################################################################
