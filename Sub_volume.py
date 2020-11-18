430f58bec29db95a5c6572924343da833a9b5f5c
Brian Lee <the930325@yahoo.com>
2020-11-18 00:00:10 -0500

    new dv ds plots

--- Sub_volume.py
+++ Sub_volume.py
@@ -11,13 +11,20 @@ from methods import *
 # RESULT:
 ######################################################################
 def main():
+    pd.set_option('display.max_rows', None)
 
-    # import pre-cleaned up csv files, 1 prolate 1 oblate
-    s_sp = pd.read_csv(s_sp_f)
-    p_gregarium = pd.read_csv(p_gregarium_f)
-    # group the two medusae of interest for easy access
-    dfs = [s_sp, p_gregarium]
-    name = ['S. sp', 'P. gregarium']
+    # import pre-cleaned up csv files, 3 oblates & 11 oblates
+    dfs = list()
+    for filename in all_csv:
+        df = pd.read_csv(filename)
+        dfs.append(df)
+
+        # group the medusa of interest for easy access
+    name = ["A. digitale", "Sarsia sp.", "P. flavicirrata", "S. meleagris", "L unguiculata",
+            "L. tetraphylla", "A. victoria", "P. gregarium", "A. aurita", "C. capillata",
+            "C. capillata"] #"M. cellularia",
+    colors = ['firebrick', 'goldenrod', 'green', 'dodgerblue', 'purple', 'red',
+              'orange', 'yellow', 'cyan', 'blue', 'pink', 'magenta']
 
     # s_sp: 0.85cm diameter, p_gregarium: 2.14cm diameter (Colin & Costello, 2001)
     s_sp_ori = ori(0.85 / 100)
@@ -25,24 +32,53 @@ def main():
     # group the two constant orifice areas for easy access
     oris = [s_sp_ori, p_gregarium_ori]
 
-    for (df, o) in zip(dfs, oris):
+    for df in dfs:
         clean_time(df)
         get_basics(df)
-        get_dsdt(df)
-        sub_n_vol_change(df, o)
+        get_thrust(df)
+        get_ds(df)
         print(df)
         print(rp.summary_cont(df['dV'], decimals=10))
 
-    dfss = [split_sp(dfs[0]), split_gregarium(dfs[1])]
+    dfss = [split_digitale(dfs[0]), split_sp(dfs[1]), split_flavicirrata(dfs[2]),
+            split_meleagris(dfs[3]), split_unguiculata(dfs[4]), split_tetraphylla(dfs[5]) +
+            split_tetraphylla2(dfs[6]), split_victoria(dfs[7]),
+            split_gregarium(dfs[9]), split_aurita(dfs[10]),
+            split_capillata(dfs[11]) + split_capillata2(dfs[12])] #split_cellularia(dfs[8]),
+
+    for df in dfss[9]:
+        for row in df.index:
+            if df.at[row, 'V'] > 1e-6:
+                df.drop(row, inplace=True)
 
     fig_label = ['A', 'B']
+    pro_done = 3
     dfs_count = 0
-    for dfs in dfss:
+    for i in range(2):
+        fig, ax = plt.subplots()
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
         total_x = np.array([])
         total_y = np.array([])
-        for df in dfs:
-            total_x = np.append(total_x, df["dV"].values)
-            total_y = np.append(total_y, df["dS"].values)
+        for dfs in dfss:
+            if pro_done != 0 and dfs_count > 2:
+                pro_done -= 1
+                continue
+            x = np.array([])
+            y = np.array([])
+            for df in dfs:
+                x = np.append(x, df["dV"].values)
+                y = np.append(y, df["dS"].values)
+
+            plt.scatter(x, y, color=colors[dfs_count], label=name[dfs_count])
+
+            total_x = np.append(total_x, x)
+            total_y = np.append(total_y, y)
+
+            dfs_count += 1
+            if dfs_count == 3:
+                break
+
         total_x2 = sm.add_constant(total_x)
         mod = sm.OLS(total_y, total_x2)
         fii = mod.fit()
@@ -52,21 +88,73 @@ def main():
         polymodel = np.poly1d(np.polyfit(total_x, total_y, 1))
         m, b = polymodel
         polyline = np.linspace(min(total_x), max(total_x), 100)
-        fig, ax = plt.subplots()
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(False)
-        plt.scatter(total_x, total_y, color='black')
-        plt.plot(polyline, polymodel(polyline), color='black', label='dV to dS linear regression')
+
+        plt.plot(polyline, polymodel(polyline), color='black')
         # chisq = sum(np.power((total_y - polymodel2(total_x)), 2)/polymodel2(total_x))
-        figtext(0.15, 0.83, fig_label[dfs_count], size=20)
+        if dfs_count > 3:
+            figtext(0.15, 0.83, fig_label[1], size=20)
+        else:
+            figtext(0.15, 0.83, fig_label[0], size=20)
         figtext(0.15, 0.76, "$r^2$: %.2f" % np.power(r[0, 1], 2))
         figtext(0.15, 0.72, "y: %.2f x + %.2E" % (m, b))
         figtext(0.15, 0.68, "p: %.2E" % p_value[1])
-        if dfs_count != 0:
-            plt.xlabel("Change of Bell Volume $(m^3)$")
+        plt.xlabel("Change of Bell Volume $(m^3)$")
         plt.ylabel("Change of Subumbrellar Volume $(m^3)$")
+        plt.legend(loc='lower right')
         plt.tight_layout()
         plt.show()
-        dfs_count += 1
+
+    fig_label = ['C', 'D']
+    for i in range(2):
+        dfs_count = 3
+        pro_done = 3
+        fig, ax = plt.subplots()
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        total_x = np.array([])
+        total_y = np.array([])
+        for dfs in dfss:
+            if pro_done != 0:
+                pro_done -= 1
+                continue
+            x = np.array([])
+            y = np.array([])
+
+            for df in dfs:
+                if i == 0:
+                    x = np.append(x, df.loc[df['dV'] >= 0, 'dV'].values)
+                    y = np.append(y, 3 * df.loc[df['dV'] >= 0, 'dS'].values)
+                if i == 1:
+                    x = np.append(x, df.loc[df['dV'] <= 0, 'dV'].values)
+                    y = np.append(y, df.loc[df['dV'] <= 0, 'dS'].values)
+
+            plt.scatter(x, y, color=colors[dfs_count], label=name[dfs_count])
+
+            total_x = np.append(total_x, x)
+            total_y = np.append(total_y, y)
+
+            dfs_count += 1
+
+        total_x2 = sm.add_constant(total_x)
+        mod = sm.OLS(total_y, total_x2)
+        fii = mod.fit()
+        p_value = fii.summary2().tables[1]['P>|t|']
+        print(fii.summary())
+        r = np.corrcoef(total_x, total_y)
+        polymodel = np.poly1d(np.polyfit(total_x, total_y, 1))
+        m, b = polymodel
+        polyline = np.linspace(min(total_x), max(total_x), 100)
+
+        plt.plot(polyline, polymodel(polyline), color='black')
+        # chisq = sum(np.power((total_y - polymodel2(total_x)), 2)/polymodel2(total_x))
+        figtext(0.15, 0.83, fig_label[i], size=20)
+        figtext(0.15, 0.76, "$r^2$: %.2f" % np.power(r[0, 1], 2))
+        figtext(0.15, 0.72, "y: %.2f x + %.2E" % (m, b))
+        figtext(0.15, 0.68, "p: %.2E" % p_value[1])
+        plt.xlabel("Change of Bell Volume $(m^3)$")
+        plt.ylabel("Change of Subumbrellar Volume $(m^3)$")
+        plt.legend(loc='lower right')
+        plt.tight_layout()
+        plt.show()
 
 main()
